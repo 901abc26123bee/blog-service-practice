b4a7b0bf553463f059d23806266f3366ec69421c
Jhenyuan Wong <tina168wong@gmail.com>
2022-07-03 21:52:35 +0800

    verify response

--- internal/routers/api/v1/article.go
+++ internal/routers/api/v1/article.go
@@ -1,14 +1,29 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"blog-service/pkg/app"
+	"blog-service/pkg/errcode"
+
+	"github.com/gin-gonic/gin"
+)
 
 type Article struct{}
 
 func NewArticle() Article {
 	return Article{}
 }
-
-func (a Article) Get(c *gin.Context) {}
+/*
+$ curl -v http://127.0.0.1:8080/api/v1/articles/1
+> GET /api/v1/articles/1 HTTP/1.1
+...
+< HTTP/1.1 500 Internal Server Error
+...
+{"code":10000000,"msg":"Server Internal Error"}%
+*/
+func (a Article) Get(c *gin.Context) {
+	app.NewResponse(c).ToErrorResponse(errcode.ServerError)
+	return
+}
 func (a Article) List(c *gin.Context) {}
 func (a Article) Create(c *gin.Context) {}
 func (a Article) Update(c *gin.Context) {}
